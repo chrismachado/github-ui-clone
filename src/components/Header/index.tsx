b1da3aa06126008d68f00db039fe00e9d8cfbe6f
Christiano Machado <christianomachado10@gmail.com>
2020-08-14 19:03:22 -0300

    feat: github public api consume

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+import { Container, GitHubLogo, SearchForm } from "./styles";
+import { useNavigate } from "react-router-dom";
+import { ThemeName } from "../../styles/themes";
+
+interface Props {
+  themeName: ThemeName;
+  setThemeName: (newName: ThemeName) => void;
+}
+
+const Header: React.FC<Props> = ({ themeName, setThemeName }) => {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+  function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+
+    navigate("/" + search.toLowerCase().trim());
+  }
+
+  function toggleTheme() {
+    setThemeName(themeName === "light" ? "dark" : "light");
+  }
+
+  return (
+    <Container>
+      <GitHubLogo onClick={toggleTheme} />
+      <SearchForm onSubmit={handleSubmit}>
+        <input
+          placeholder="Enter username or repo"
+          value={search}
+          onChange={(e) => setSearch(e.currentTarget.value)}
+        />
+      </SearchForm>
+    </Container>
+  );
+};
+
+export default Header;
